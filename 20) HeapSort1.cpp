ec7c7d85501d1471b450a93e56acee9011b2d5fa
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2025-02-09 10:44:36 +0530

    Create 20) HeapSort1.cpp

---
+++ 20) HeapSort1.cpp
@@ -0,0 +1,74 @@
+// Implement heap sort.
+
+#include<iostream>
+using namespace std;
+
+void heapify(int* arr, int n, int i)
+{
+	int largest = i;
+	int left = 2*i + 1;
+	int right = 2*i + 2;
+
+	if(left < n && arr[left] > arr[largest])
+	{
+		largest = left;
+	}
+
+	if(right < n && arr[right] > arr[largest])
+	{
+		largest = right;
+	}
+
+	if(largest != i)
+	{
+		swap(arr[largest], arr[i]);
+
+		heapify(arr, n, largest);
+	}
+}
+
+void heapsort(int* arr, int n)
+{
+	for(int i= n/2-1; i>=0; i--)
+	{
+		heapify(arr, n, i);
+	}
+
+	for(int i =n-1; i>=0; i--)
+	{
+		swap(arr[0], arr[i]);
+
+		heapify(arr, i, 0);
+	}
+}
+
+void printArray(int* arr, int n)
+{
+	cout<<"sorted list : ";
+	for(int i=0; i<n; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
+
+int main()
+{
+	int n;
+	cout<<"enter the number of elements:  ";
+	cin>>n;
+
+	int* arr = new int[n];
+
+	int ele;
+	cout<<"Enter "<<n<<" elements "<<endl;
+	for(int i=0; i<n; i++)
+	{
+		cin>>ele;
+		arr[i] = ele;
+	}
+
+	heapsort(arr, n);
+
+	printArray(arr, n);
+
+}
